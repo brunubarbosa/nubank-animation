43f4f1b27fcf86cbc2642fb4e19aeb49ecf0296f
Bruno <brunopjsw@gmail.com>
2019-06-22 18:10:58 -0300

    add depencies

---
+++ src/components/Header/styles.js
@@ -0,0 +1,22 @@
+import Styled from 'styled-components';
+
+export const Container = Styled.View`
+    align-items: center;
+    padding: 40px 0 30px;
+`;
+
+
+
+export const Top = Styled.View`
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 10px;
+`;
+export const Logo = Styled.Image``;
+
+export const Title = Styled.Text`
+    font-size: 18px;
+    color: #FFF;
+    font-weight: bold;
+    margin-left: 8px;
+`;
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,6 @@
+import Styled from 'styled-components';
+
+export const Container = Styled.View`
+    flex: 1;
+    background: #8810AE;
+`
